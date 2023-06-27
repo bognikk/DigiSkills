a6ad81d6ed450c9e012e83282022356b4e2ff63e
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-06-27 14:18:45 -0400

    quick links

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
+import QuickLinks from "./components/QuickLinks/QuickLinks";
+
 const App = () => {
 	return (
 		<>
 			<Header />
 			<Hero />
+			<QuickLinks />
 		</>
 	);
 };
---
+++ src/components/QuickLinks/QuickLinks.jsx
@@ -0,0 +1,85 @@
+import "./QuickLinks.scss";
+
+const QuickLinks = () => {
+	return (
+		<section className="quick-links">
+			<h2 className="quick-links__title">Quick links</h2>
+			<ul className="quick-links__content">
+				<li>
+					<a href="#top" className="ql-01">
+						<div className="icon"></div>
+						<div className="text">
+							<h4>Perfect Training Match</h4>
+							<p>Take the test & find your training</p>
+						</div>
+					</a>
+				</li>
+				<li>
+					<a href="#top" className="ql-02">
+						<div className="icon"></div>
+						<div className="text">
+							<h4>Inspiring Digital Projects</h4>
+							<p>Belgian Initiatives from A to Z</p>
+						</div>
+					</a>
+				</li>
+				<li>
+					<a href="#top" className="ql-03">
+						<div className="icon"></div>
+						<div className="text">
+							<h4>Funding for Your Business</h4>
+							<p>Kickstart your digital change</p>
+						</div>
+					</a>
+				</li>
+				<li>
+					<a href="#top" className="ql-04">
+						<div className="icon"></div>
+						<div className="text">
+							<h4>Lorem Ipsum dol</h4>
+							<p>Lorem Ipsum dol</p>
+						</div>
+					</a>
+				</li>
+				<li>
+					<a href="#top" className="ql-05">
+						<div className="icon"></div>
+						<div className="text">
+							<h4>Activities for Kids & Youth</h4>
+							<p>Never too young to start learning!</p>
+						</div>
+					</a>
+				</li>
+				<li>
+					<a href="#top" className="ql-06">
+						<div className="icon"></div>
+						<div className="text">
+							<h4>Digital Tools for Beginners</h4>
+							<p>Start from scratch & make it last</p>
+						</div>
+					</a>
+				</li>
+				<li>
+					<a href="#top" className="ql-07">
+						<div className="icon"></div>
+						<div className="text">
+							<h4>News in Belgium</h4>
+							<p>Our changing digital landscape</p>
+						</div>
+					</a>
+				</li>
+				<li>
+					<a href="#top" className="ql-08">
+						<div className="icon"></div>
+						<div className="text">
+							<h4>Lorem Ipsum dol</h4>
+							<p>Lorem Ipsum dol</p>
+						</div>
+					</a>
+				</li>
+			</ul>
+		</section>
+	);
+};
+
+export default QuickLinks;
